b8b300665a43681b9cc2e0b64e472faed9cc7168
Md Khan <md.monir.khan707@gmail.com>
2022-03-22 18:28:52 -0400

    Added username to users table and solve the n+1 problem
    Created urls with the author name
    Solved the n+1 problem with the following code
    'posts' => $category->posts->load(['category', 'author'])
    'posts' => $author->posts->load(['category', 'author'])

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -4,7 +4,7 @@
             {{$post->title }}
         </h1>
         <p>
-            By <a href="/authors/{{ $post->author->name }}"> {{ $post->author->name }} </a>
+            By <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} </a>
             in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }} </a>
         </p>
 
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -89,14 +89,16 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        // 'posts' => $category->posts # This line cases the n+1 problem
+        'posts' => $category->posts->load(['category', 'author'])  # this is the solutuon
     ]);
 });
 
 # author
-Route::get('/authors/{author}', function (User $author) {
-    
+Route::get('/authors/{author:username}', function (User $author) {
+
     return view('posts', [
-        'posts' => $author->posts
+        //'posts' => $author->posts  # This line cases the n+1 problem
+        'posts' => $author->posts->load(['category', 'author']) # this is the solutuon
     ]);
 });
